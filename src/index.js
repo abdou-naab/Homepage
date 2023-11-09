4d191de626490bd7bf894b8832c9f1ae5371b4a0
abdou <113465542+abdou-naab@users.noreply.github.com>
2023-11-09 11:17:33 +0100

    pretty much done with the first look

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import styles from "./styles.css";
 console.log("Assalam Alaikom");
+
+const navLinks = document.querySelectorAll("header nav a");
+const sections = document.querySelectorAll(".content section");
+
+function highlightActiveLink() {
+  const scrollPosition = window.scrollY + 5;
+  const atBottom =
+    window.innerHeight + scrollPosition >= document.body.offsetHeight;
+  sections.forEach((section, index) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (
+      (scrollPosition >= sectionTop &&
+        scrollPosition < sectionTop + sectionHeight) ||
+      (atBottom && index == sections.length - 1)
+    ) {
+      navLinks.forEach((link) => link.classList.remove("active-a"));
+      navLinks[index].classList.add("active-a");
+    }
+  });
+}
+window.addEventListener("scroll", highlightActiveLink);
